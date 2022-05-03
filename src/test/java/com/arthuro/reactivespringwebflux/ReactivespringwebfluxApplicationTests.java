70c368e17531dd2137e524230c6f4c3d83a840d0
Nodji Junior <43173093+nodjijr@users.noreply.github.com>
2022-05-03 11:31:43 -0300

    Update ReactivespringwebfluxApplicationTests.java

--- src/test/java/com/arthuro/reactivespringwebflux/ReactivespringwebfluxApplicationTests.java
+++ src/test/java/com/arthuro/reactivespringwebflux/ReactivespringwebfluxApplicationTests.java
@@ -1,13 +1,32 @@
 package com.arthuro.reactivespringwebflux;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.arthuro.reactivespringwebflux.handler.UserHandler;
+import com.arthuro.reactivespringwebflux.repository.UserRepository;
+import com.arthuro.reactivespringwebflux.service.UserService;
 
 @SpringBootTest
 class ReactivespringwebfluxApplicationTests {
 
+	@Autowired
+	private UserHandler controller;
+
+	@Autowired
+	private UserService service;
+
+	@Autowired
+	private UserRepository repository;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
+		assertThat(service).isNotNull();
+		assertThat(repository).isNotNull();
 	}
 
 }
