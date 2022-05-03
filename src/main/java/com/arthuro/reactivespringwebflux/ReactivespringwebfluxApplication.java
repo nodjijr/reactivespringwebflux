68c7bc097212556bc146fb9680b6f5838f586e85
Nodji Junior <43173093+nodjijr@users.noreply.github.com>
2022-05-03 11:33:15 -0300

    Update ReactivespringwebfluxApplication.java

--- src/main/java/com/arthuro/reactivespringwebflux/ReactivespringwebfluxApplication.java
+++ src/main/java/com/arthuro/reactivespringwebflux/ReactivespringwebfluxApplication.java
@@ -2,6 +2,12 @@ package com.arthuro.reactivespringwebflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class ReactivespringwebfluxApplication {
@@ -10,4 +16,9 @@ public class ReactivespringwebfluxApplication {
 		SpringApplication.run(ReactivespringwebfluxApplication.class, args);
 	}
 
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
+	}
 }
